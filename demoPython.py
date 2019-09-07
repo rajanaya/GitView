5a9a581a8fd66f2154574ce7276e669d9ae85063
rajanaya <48748374+rajanaya@users.noreply.github.com>
2019-09-07 21:34:12 +0530

    my final statment about python

--- demoPython.py
+++ demoPython.py
@@ -1,2 +1,5 @@
 print("I am totally new to Python\n As I came to know it's new and has a good demand I started using it")
 print("Thankfully I got some good videos in youtube")
+print("Hopefully it will be useful in my carrier")
+print("Thanks all for sharing some valuable time with me")
+print("miss u all a lot")
